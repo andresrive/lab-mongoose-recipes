da03db957d815c134804fc5f2e4bfcffd94368a1
Andres Rivera <andreseriverar@gmail.com>
2023-02-10 18:12:37 +0100

    lab completed

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
+const Movie = require('../../W4_D5/models/movie.model');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
@@ -12,11 +13,70 @@ mongoose
   .connect(MONGODB_URI)
   .then(x => {
     console.log(`Connected to the database: "${x.connection.name}"`);
+
+    mongoose.connection.on("disconnected", () => {
+      console.log("Se ha desconectado de la base de datos")
+    })
+    mongoose.connection.on("error", () => {
+      console.log("Error en la base de datos")
+    })
+    process.on("SIGINT", () => {
+      mongoose.disconnect();
+      process.kill(process.pid)
+    })
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    return Recipe.create({
+      "title": "Japanese Glazed Chicken Thighs",
+      "level": "Amateur Chef",
+      "ingredients": [
+        "1/2 cup rice vinegar",
+        "5 tablespoons honey",
+        "1/3 cup soy sauce (such as Silver Swan®)",
+        "1/4 cup Asian (toasted) sesame oil",
+        "3 tablespoons Asian chili garlic sauce",
+        "3 tablespoons minced garlic",
+        "salt to taste",
+        "8 skinless, boneless chicken thighs"
+      ],
+      "cuisine": "Asian",
+      "dishType": "main_course",
+      "image": "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+      "duration": 40,
+      "creator": "Chef LePapu"
+    })
+  })
+  .then((result) => {
+    console.log("Console.log de create", result.title)
+  })
+  .then(() => {
+    return Recipe.insertMany(data)
+  })
+  .then(recipes => {
+    let titles
+    for (let i = 0; i < recipes.length; i++) {
+      titles = titles + ", " + recipes[i].title
+    }
+    console.log("Console.log de insertMany", titles)
+  })
+  .then(() => {
+    return Recipe.findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { duration: 100 }, { new: true })
+  })
+  .then(result => {
+    console.log("Console.log de findOneAndUpdate", result)
+    console.log("Success!")
+  })
+  .then(() => {
+    return Recipe.deleteOne({ title: "Carrot Cake" })
+  })
+  .then((result) => {
+    console.log(result)
+    console.log("Carrot cake removed!")
+  })
+  .then(() => {
+    mongoose.disconnect()
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
